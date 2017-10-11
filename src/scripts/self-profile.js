32e27cdbd168e1f40785b00bbe976784367cb4ba
Elton Xue <eltonxue@gmail.com>
2017-10-11 15:03:52 -0700

    finished self-profile page

---
+++ src/scripts/self-profile.js
@@ -0,0 +1,3 @@
+document.getElementById('discover').onclick = function() {
+  window.location.href = 'discovery.html';
+};
---
+++ src/scripts/signup-login-page.js
@@ -0,0 +1,7 @@
+document.getElementById('signup').onclick = function() {
+  window.location.href = 'self-profile.html';
+};
+
+document.getElementById('login').onclick = function() {
+  window.location.href = 'self-profile.html';
+};
