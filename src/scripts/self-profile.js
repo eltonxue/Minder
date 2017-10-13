496b9117b22357428b6a678076ecd8d337c120d1
Elton Xue <eltonxue@gmail.com>
2017-10-13 02:08:23 -0700

    attempt @ editting profile

--- src/scripts/self-profile.js
+++ src/scripts/self-profile.js
@@ -1,3 +1,27 @@
 document.getElementById('discover').onclick = function() {
   window.location.href = 'discovery.html';
 };
+
+const editDescription = document.getElementById('edit-description');
+const editEducation = document.getElementById('edit-education');
+const editTags = document.getElementById('edit-tags');
+const editLocation = document.getElementById('edit-location');
+
+editDescription.addEventListener('click', function(event) {
+  var element = document.getElementById('description-text');
+  if (element != null) {
+    var text = element.innerHTML;
+
+    var textArea = document.createElement('textarea');
+    textArea.id = 'description-textarea';
+    textArea.appendChild(document.createTextNode(text));
+
+    editDescription.replaceChild(textArea, element);
+  }
+});
+
+editEducation.addEventListener('click', function(event) {});
+
+editTags.addEventListener('click', function(event) {});
+
+editLocation.addEventListener('click', function(event) {});
